21c82fd97a405e6484ef7e210c8d8b252d8d2c46
Spencer Gibson <spencergibson26@gmail.com>
2021-03-24 10:43:25 -0700

    Initial Commit

---
+++ CovidSafe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidSafe
+//
+//  Created by Spencer Gibson on 3/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CovidSafe/CovidSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidSafeApp.swift
+//  CovidSafe
+//
+//  Created by Spencer Gibson on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct CovidSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
