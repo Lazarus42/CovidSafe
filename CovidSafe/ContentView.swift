ff5f32479a15e7b28a1d156f3fb934b32f443795
Spencer Gibson <spencergibson26@gmail.com>
2021-04-12 19:39:31 -0700

    Update ContentView.swift

--- CovidSafe/ContentView.swift
+++ CovidSafe/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, worldd, hello  everyone!!!")
+        Text("Hello, world!")
             .padding()
     }
 }
